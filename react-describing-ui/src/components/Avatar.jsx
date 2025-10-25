4d4b0ed3fed9c40d6db0e8a6303ef7746d0510fd
Your Name <you@example.com>
2025-10-25 19:56:15 +0800

    Done with how to configure components with props

---
+++ react-describing-ui/src/components/Avatar.jsx
@@ -0,0 +1,15 @@
+import { getImageUrl } from "../utility/utils.js";
+
+function  Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default Avatar;

2a025882d256984b04762812684303c8af2612d0
Your Name <you@example.com>
2025-11-09 17:44:45 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -3,13 +3,15 @@ export default function TodoList() {
     <>
     <h1>Hedy Lamarr's Todos</h1>
     <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
-      class="photo" />
-    <ul>
-      <li>Invent new traffic lights</li>
-      <li>Rehearse a movie scene</li>
-      <li>Improve the spectrum technology</li>
+    src="https://i.imgur.com/yXOvdOSs.jpg" 
+    alt="Hedy Lamarr" 
+    class="photo"
+   />
+  <ul>
+    <li>Invent new traffic lights</li>
+    <li>Rehearse a movie scene</li>
+    <li>Improve the spectrum technology</li>
+
     </ul>
     </>
   );

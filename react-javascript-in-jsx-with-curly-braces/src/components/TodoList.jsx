cba0f19767ef18a2830c0b53f670891d4ce09b68
Your Name <you@example.com>
2025-11-23 17:02:44 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/components/TodoList.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/components/TodoList.jsx
@@ -1,6 +1,12 @@
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
   return (
-    <h1>{name}'s To Do List</h1>
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
   );
 }
